8394eb038cb76cc92163063aec69670ad15fc2de
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2014-10-19 13:15:39 +0200

    Fix ResourceStream formatting.

--- src/VBE/system/android/ResourceStream.cpp
+++ src/VBE/system/android/ResourceStream.cpp
@@ -1,37 +1,26 @@
-#include "ResourceStream.hpp"
+#include <VBE/system/android/ResourceStream.hpp>
 
-ResourceStream::ResourceStream(const std::string& filename) : asset(nullptr)
-{
+ResourceStream::ResourceStream(const std::string& filename) : asset(nullptr) {
 	ActivityStates* states = getActivity(NULL);
 	asset = AAssetManager_open(states->activity->assetManager, filename.c_str(), AASSET_MODE_UNKNOWN);
 }
 
-
-ResourceStream::~ResourceStream()
-{
+ResourceStream::~ResourceStream() {
 	AAsset_close(asset);
 }
 
-
-long int ResourceStream::read(void *data, long int size)
-{
+long int ResourceStream::read(void *data, long int size) {
 	return AAsset_read(asset, data, size);
 }
 
-
-long int ResourceStream::seek(long int position)
-{
+long int ResourceStream::seek(long int position) {
 	AAsset_seek(asset, position, SEEK_SET);
 }
 
-
-long int ResourceStream::tell()
-{
+long int ResourceStream::tell() {
 	return getSize() - AAsset_getRemainingLength(asset);
 }
 
-
-long int ResourceStream::getSize()
-{
+long int ResourceStream::getSize() {
 	return AAsset_getLength(asset);
 }
