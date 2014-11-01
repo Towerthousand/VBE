0fb9666edbf2f43a97f355f6a1921e810c6b501d
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2014-11-01 14:48:38 +0100

    Check that assets are opened properly.

--- src/VBE/system/sdl2/StorageImpl.cpp
+++ src/VBE/system/sdl2/StorageImpl.cpp
@@ -1,10 +1,14 @@
 #include <fstream>
 
 #include <VBE/system/sdl2/StorageImpl.hpp>
+#include <VBE/system/Log.hpp>
 
 // static
 std::unique_ptr<std::istream> StorageImpl::openAsset(const std::string& filename) {
 	// Open in binary mode so Windows doesn't change LF to CRLF,
 	// which will corrupt binary files such as images.
-	return std::unique_ptr<std::istream>(new std::ifstream("assets/"+filename, std::ios::binary));
+	std::ifstream* stream = new std::ifstream("assets/"+filename, std::ios::binary);
+
+	VBE_ASSERT(stream->good(), "Could not open asset: "+filename);
+	return std::unique_ptr<std::istream>(stream);
 }
